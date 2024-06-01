6e434c12ca65e82e56167a513c73189dfe214aa5
Juan Ayme(Maver) <juanayme565@gmail.com>
2024-06-01 16:02:58 -0500

    cambio del dolar a soles

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/tiendaponti/tienda-ponti/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "12345";
+const DB = "mitienda";
+const CHARSET = "charset=utf8";
+const TITLE = "SHOP";
+const MONEDA = "USD";
+const CLIENT_ID = "ASiWxZQfFLhRQ2sInpSjHhO_7CEXGv0gNYjMjqFJog-5SxrCqpKkt4UMEUz_NB442yHq-ddW25Lwuucu";
+?>
